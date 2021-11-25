38f9c45dc5e976725c059b701aa463cd6afc13b6
minjeongnew <20171623sw@kookmin.ac.kr>
2021-11-25 23:02:43 +0900

    programmers level1 수학/나머지가 1이 되는 수 찾기

---
+++ programmers/level1/나머지가_1이_되는_수_찾기.cpp
@@ -0,0 +1,15 @@
+//
+// Created by park on 21. 11. 25..
+//
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 2 ; i < n ; i++) {
+        if (n % i==1) return i;
+    }
+}
